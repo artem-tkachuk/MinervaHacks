e8a877423fd3878c3232f28bcd4b19671365a22a
NEsfandiari <nesfandiari37@gmail.com>
2019-04-06 16:22:09 -0700

    react native timer module

--- MinervaMobileApp/App.js
+++ MinervaMobileApp/App.js
@@ -1,30 +1,58 @@
 import React from "react";
 import { StyleSheet, Text, View, Button, AlertIOS } from "react-native";
+import { Stopwatch, Timer } from "react-native-stopwatch-timer";
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { time: 0 };
+    this.state = {
+      time: 0,
+      timerStart: false
+    };
   }
-  onPressLearnMore = () => {
-    AlertIOS.alert("Sync Complete", "All your data are belong to us.");
+  toggleTimer = () => {
+    const newState = !this.state.timerStart;
+    this.setState({ timerStart: newState });
+  };
+  getFormattedTime = () => {
+    return this.state.time;
   };
   render() {
     return (
       <View style={styles.container}>
         <Button
-          title={"Im Waiting For The Bus (Start Button)"}
-          onPress={this.onPressLearnMore}
+          title={!this.state.timerStart ? "Start" : "Stop"}
+          onPress={this.toggleTimer}
           color="#5AB14C"
         />
         <View style={styles.timer}>
-          <Text>{this.state.time}</Text>
+          <Timer
+            start={this.state.timerStart}
+            // reset={this.state.timerReset}
+            // options={options}
+            // handleFinish={handleTimerComplete}
+            getTime={this.getFormattedTime}
+          />
           <Text>Timer Here</Text>
         </View>
       </View>
     );
   }
 }
+
+const options = {
+  container: {
+    backgroundColor: "#000",
+    padding: 5,
+    borderRadius: 5,
+    width: 220
+  },
+  text: {
+    fontSize: 30,
+    color: "#FFF",
+    marginLeft: 7
+  }
+};
 
 const styles = StyleSheet.create({
   container: {
