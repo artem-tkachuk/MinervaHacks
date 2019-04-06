92986b80433c284e8b28b339d3d172b3ac57f9ad
NEsfandiari <nesfandiari37@gmail.com>
2019-04-06 15:01:12 -0700

    mobile app

---
+++ MinervaMobileApp/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Im Waiting For The Bus (Start Button)</Text>
+        <View>
+          <Text>Timer Here</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
